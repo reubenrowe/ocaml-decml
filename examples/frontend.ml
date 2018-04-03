56ef8b6d1ff7a0a7545c56a692c06a3f3b9c3e66
Reuben Rowe <r.n.s.rowe@kent.ac.uk>
2018-04-03 16:36:13 +0100

    Examples.

---
+++ examples/frontend.ml
@@ -0,0 +1,66 @@
+open Decml
+
+let data_file = ref None
+let iterations = ref 50000
+let threshold = ref 1.0
+let rate = ref 0.00001
+let start_x = ref 0.0
+let end_x = ref 10.0
+let step = ref 1.0
+
+let speclist = [
+    ("-data-file", Arg.String (fun s -> data_file := Some s), 
+      ": file from which to read data samples") ;
+    ("-iterations", Arg.Int (fun i -> iterations := i), 
+      Format.sprintf 
+        ": maximum number of iterations to perform during model optimisation \
+           (default = %i)" !iterations) ;
+    ("-threshold", Arg.Float (fun f -> threshold := f),
+      Format.sprintf
+        ": desired error threshold for optimisation (default = %f)"
+        !threshold) ;
+    ("-learning-rate", Arg.Float (fun f -> rate := f), 
+      Format.sprintf ": learning rate (default %f)" !rate) ;
+    ("-start", Arg.Float (fun f -> start_x := f), 
+      Format.sprintf 
+        ": initial [x] value for output of optimised model (default = %f)" 
+        !start_x) ;
+    ("-end", Arg.Float (fun f -> end_x := f), 
+      Format.sprintf 
+        ": final [x] value for output of optimised model (default = %f)" !end_x) ;
+    ("-step", Arg.Float (fun f -> step := f), 
+      Format.sprintf 
+        ": [x] step value for output of optimised model (default = %f)" !step) ;
+  ]
+
+let usage = "usage: [-iterations <int>] \
+                    [-threshold <float>] \
+                    [-learning-rate <float>] \
+                    [-start <float>] \
+                    [-end <float>] \
+                    [-step <float>] \
+                    -data-file <file>"
+
+let () = Arg.parse speclist (fun _ -> ()) usage
+
+let data =
+  match !data_file with
+  | None ->
+    failwith "Must specify a data file!"
+  | Some f ->
+    let load =
+      Data.of_csv_file
+        (List.map 
+          (fun xs -> 
+            let x = float_of_string (List.nth xs 0) in
+            let y = float_of_string (List.nth xs 1) in
+            x, y)) in
+    load f
+
+let rec output f x =
+  if Containers.Float.(x <= !end_x) then
+    let () = 
+      print_endline
+        (Format.sprintf "%f %f" x (f x)) in
+    output f (x +. !step)
+    
--- examples/linear_regression.ml
+++ examples/linear_regression.ml
@@ -4,84 +4,17 @@ open Decml
 open   Model
 open   Optimise
 
-let data_file = ref None
-let iterations = ref 5000
-let rate = ref 0.00001
-let start_x = ref 0.0
-let end_x = ref 10.0
-let step = ref 1.0
+open Frontend
 
-let speclist = [
-    ("-data-file", Arg.String (fun s -> data_file := Some s), 
-      ": file from which to read data samples") ;
-    ("-iterations", Arg.Int (fun i -> iterations := i), 
-      Format.sprintf 
-        ": number of iterations to perform during model optimisation \
-           (default = %i)" !iterations) ;
-    ("-learning-rate", Arg.Float (fun f -> rate := f), 
-      Format.sprintf ": learning rate (default %f)" !rate) ;
-    ("-start", Arg.Float (fun f -> start_x := f), 
-      Format.sprintf 
-        ": initial [x] value for output of optimised model (default = %f)" 
-        !start_x) ;
-    ("-end", Arg.Float (fun f -> end_x := f), 
-      Format.sprintf 
-        ": final [x] value for output of optimised model (default = %f)" !end_x) ;
-    ("-step", Arg.Float (fun f -> step := f), 
-      Format.sprintf 
-        ": [x] step value for output of optimised model (default = %f)" !step) ;
-  ]
-
-let usage = "usage: [-iterations <int>] \
-                    [-learning-rate <float>] \
-                    [-start <float>] \
-                    [-end <float>] \
-                    [-step <float>] \
-                    -data-file <file>"
-
-let () = Arg.parse speclist (fun _ -> ()) usage
-
-let data =
-  match !data_file with
-  | None ->
-    failwith "Must specify a data file!"
-  | Some f ->
-    let load =
-      Data.of_csv_file
-        (List.map 
-          (fun xs -> 
-            let x = float_of_string (List.nth xs 0) in
-            let y = float_of_string (List.nth xs 1) in
-            x, y)) in
-    load f
-
-(* model m x = 1.0[@pc] * x + 0.0[@pc] *)
-let model =
-  let open Overlay.Pervasives in
-  abs 
-    (app
-      (app 
-        (tx Weak (+.)) 
-        (app 
-          (app 
-            (tx Weak ( *. )) 
-            (tx Weak (pc ~init:"1.0" ()))) 
-          (var))) 
-      (tx Weak (pc ~init:"0.0" ())))
+let%model linear x = [%pc 1.0] *. x +. [%pc 0.0]
 
 let model =
-  let Ex (m, p) = model in
+  let%decouple (m, p) = linear in
   let m = rebind m in
   let p = 
-    grad_desc ~loss_f:mse ~rate:!rate ~epochs:!iterations ~model:m p data in
+    grad_desc ~loss_f:mse ~rate:!rate ~threshold:!threshold ~epochs:!iterations 
+      ~model:m p data in
   m p
-
-let rec output f x =
-  if Float.compare x !end_x <= 0 then
-    let () = 
-      print_endline
-        (Format.sprintf "x = %f, y = %f" x (f x)) in
-    output f (x +. !step)
 
 let () =
   output model !start_x
