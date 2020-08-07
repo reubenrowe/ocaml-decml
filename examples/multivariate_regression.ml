b87d561ee4b0f34cbfea0570a6f750bdb3c965bc
Reuben Rowe <reuben.rowe@rhul.ac.uk>
2020-08-07 15:31:18 +0100

    Split data into training and test sets for multivariate regression example.

--- examples/multivariate_regression.ml
+++ examples/multivariate_regression.ml
@@ -16,6 +16,9 @@ let data =
       %> hd_tl
       %> map2 (fun x -> 1.0 :: x)
       %> swap)
+  
+let (training_data, test_data) =
+  take_drop (((length data) / 10) * 9) data
 
 let rec mk_vector n =
   if n < 0 then
@@ -23,25 +26,30 @@ let rec mk_vector n =
   else if n = 0 then
     [%model [] ]
   else
-    (* fmap (fun (hd, tl) -> hd :: tl) (pair ([%pc 1], (mk_vector (n-1)))) *)
-    app (fmap List.cons [%pc 1]) (mk_vector (n-1))
-
+    (* fmap (fun (hd, tl) -> hd :: tl) (pair ([%pc 0], (mk_vector (n-1)))) *)
+    app (fmap List.cons [%pc 0]) (mk_vector (n-1))
 
 let mvr =
   let%lift dot = List.fold_left2 (fun dp x y -> dp +. (x *. y)) 0.0 in
-  let w = 
-    (* We get very different results depending on whether we use Model.pv or
-       the mk_vector function defined above - why is this? *)
-    pv 14 in
+  let w = pv 14 in
   [%model fun x -> dot w x ]
 
 let model =
   let%decouple (model, params) = mvr in
   let params = 
     grad_desc ~loss_f:mse ~rate:!rate ~threshold:!threshold ~epochs:!iterations 
-      ~model params data in
+      ~model params training_data in
   rebind model params
 
-(* let () =
-  output model Float.pp !start_x *)
+let () =
+  let output_data_point (input, expected) =
+    let predicted = model input in
+    print_endline
+      (Format.sprintf
+        "@[<h>data point: (%a)@.\texpected: %f, predicted: %f@]"
+          (Format.list Format.pp_print_float) input
+          expected
+          predicted)
+    in
+  List.iter output_data_point test_data
   
